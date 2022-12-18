732700561e7794afb9f2227c39c9e374f6414036
laulexa <laura.alexandra.roa@gmail.com>
2022-12-18 00:20:02 -0800

    second commit

--- my-app/src/TodoSearch.js
+++ my-app/src/TodoSearch.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import './TodoSearch.css';
 
-function TodoSearch () {
-    return (
-        <input placeholder="cebolla"/>
-    )
+function TodoSearch() {
+  return (
+    <input className="TodoSearch" placeholder="Cebolla" />
+  );
 }
- export { TodoSearch }
+
+export { TodoSearch };
