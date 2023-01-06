7b3901966fba70e2182a449bfb0da1b33ccf7aab
laulexa <laura.alexandra.roa@gmail.com>
2023-01-05 23:55:54 -0800

    local storage part1

--- my-app/src/App/index.js
+++ my-app/src/App/index.js
@@ -2,15 +2,25 @@ import React from 'react';
 import { AppUI } from './AppUI';
 // import './App.css';
 
-const defaultTodos = [
-  { text: 'Cortar cebolla', completed: true },
-  { text: 'Tomar el cursso de intro a React', completed: false },
-  { text: 'Llorar con la llorona', completed: false },
-  { text: 'LALALALAA', completed: false },
-];
+// const defaultTodos = [
+//   { text: 'Cortar cebolla', completed: true },
+//   { text: 'Tomar el cursso de intro a React', completed: false },
+//   { text: 'Llorar con la llorona', completed: false },
+//   { text: 'LALALALAA', completed: false },
+// ];
 
 function App() {
-  const [todos, setTodos] = React.useState(defaultTodos);
+const localStorageTodos = localStorage.getItem('TODOS_V1');
+let parsedTodos;
+
+if(!localStorageTodos) {
+  localStorage.setItem('TODOS_V1', JSON.stringify([]));
+  parsedTodos = [];
+} else {
+  parsedTodos = JSON.parse(localStorageTodos);
+}
+
+  const [todos, setTodos] = React.useState(parsedTodos);
   // El estado de nuestra búsqueda
   const [searchValue, setSearchValue] = React.useState('');
 
@@ -30,11 +40,17 @@ function App() {
     });
   }
   
+  const saveTodos = (newTodos) => {
+    const stringifiedTodos = JSON.stringify(newTodos);
+    localStorage.setItem('TODOS_V1', stringifiedTodos);
+    setTodos(newTodos);
+  };
+
   const completeTodo = (text) => {
     const todoIndex = todos.findIndex(todo => todo.text === text);
     const newTodos = [...todos];
     newTodos[todoIndex].completed = true;
-    setTodos(newTodos);
+    saveTodos(newTodos);
       // todos[todoIndex] = {
       // text: todos[todoIndex].text,
       // completed: true,
@@ -45,7 +61,7 @@ function App() {
     const todoIndex = todos.findIndex(todo => todo.text === text);
     const newTodos = [...todos];
     newTodos.splice(todoIndex, 1);
-    setTodos(newTodos);
+    saveTodos(newTodos);
   };
 
   return (
